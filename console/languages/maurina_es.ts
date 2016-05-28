c803889a6ee01f1358aecab89081757db6e046fb
Koas <alvaro.calleja@gmail.com>
2016-05-28 18:43:02 +0200

    i18n file for v1.2

--- console/languages/maurina_es.ts
+++ console/languages/maurina_es.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="es_ES">
+<TS version="2.0" language="es_ES">
 <context>
     <name>MainWindow</name>
     <message>
@@ -10,15 +10,15 @@
     </message>
     <message>
         <source>Server</source>
-        <translation type="vanished">Servidor</translation>
+        <translation>Servidor</translation>
     </message>
     <message>
         <source>Port</source>
-        <translation type="vanished">Puerto</translation>
+        <translation>Puerto</translation>
     </message>
     <message>
         <source>Restart</source>
-        <translation type="vanished">Reiniciar</translation>
+        <translation>Reiniciar</translation>
     </message>
     <message>
         <location filename="../MainWindow.ui" line="490"/>
@@ -53,32 +53,32 @@
     <message>
         <location filename="../MainWindow.ui" line="188"/>
         <source>Log 5</source>
-        <translation type="unfinished">Log 5</translation>
+        <translation>Log 5</translation>
     </message>
     <message>
         <location filename="../MainWindow.ui" line="258"/>
         <source>Label A</source>
-        <translation type="unfinished"></translation>
+        <translation>Campo A</translation>
     </message>
     <message>
         <location filename="../MainWindow.ui" line="285"/>
         <source>Label B</source>
-        <translation type="unfinished"></translation>
+        <translation>Campo B</translation>
     </message>
     <message>
         <location filename="../MainWindow.ui" line="341"/>
         <source>Label C</source>
-        <translation type="unfinished"></translation>
+        <translation>Campo C</translation>
     </message>
     <message>
         <location filename="../MainWindow.ui" line="368"/>
         <source>Label D</source>
-        <translation type="unfinished"></translation>
+        <translation>Campo D</translation>
     </message>
     <message>
         <location filename="../MainWindow.ui" line="391"/>
         <source>Label E</source>
-        <translation type="unfinished"></translation>
+        <translation>Campo E</translation>
     </message>
     <message>
         <location filename="../MainWindow.ui" line="470"/>
@@ -88,7 +88,7 @@
     <message>
         <location filename="../MainWindow.ui" line="504"/>
         <source>seconds</source>
-        <translation type="unfinished"></translation>
+        <translation>segundos</translation>
     </message>
     <message>
         <location filename="../MainWindow.ui" line="533"/>
@@ -108,40 +108,40 @@
     <message>
         <location filename="../MainWindow.ui" line="560"/>
         <source>&amp;Hide controls</source>
-        <translation type="unfinished"></translation>
+        <translation>Esconder &amp;controles</translation>
     </message>
     <message>
         <location filename="../MainWindow.ui" line="565"/>
         <source>Change &amp;layout</source>
-        <translation type="unfinished"></translation>
+        <translation>Cambiar &amp;distribución</translation>
     </message>
     <message>
         <location filename="../MainWindow.ui" line="570"/>
         <source>&amp;Preferences</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Preferencias</translation>
     </message>
     <message>
         <location filename="../MainWindow.ui" line="575"/>
         <source>Change screen &amp;mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Cambiar &amp;modo de pantalla</translation>
     </message>
     <message>
         <location filename="../MainWindow.cpp" line="65"/>
         <location filename="../MainWindow.cpp" line="443"/>
         <source>Ctrl+P</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+P</translation>
     </message>
     <message>
         <location filename="../MainWindow.cpp" line="66"/>
         <location filename="../MainWindow.cpp" line="423"/>
         <source>Ctrl+H</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+H</translation>
     </message>
     <message>
         <location filename="../MainWindow.cpp" line="67"/>
         <location filename="../MainWindow.cpp" line="438"/>
         <source>Ctrl+L</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+L</translation>
     </message>
     <message>
         <location filename="../MainWindow.cpp" line="130"/>
@@ -182,7 +182,25 @@ p, li { white-space: pre-wrap; }
 &lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;This software is free as in free beer and free speech.&lt;/span&gt;&lt;/p&gt;
 &lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
 &lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;If you find this program useful please consider &lt;/span&gt;&lt;a href=&quot;http://www.maurina.org/donate&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; text-decoration: underline; color:#0000ff;&quot;&gt;making a donation&lt;/span&gt;&lt;/a&gt;&lt;span style=&quot; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;.&lt;/span&gt;&lt;/p&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;.SF NS Text&apos;; font-size:13pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;table border=&quot;0&quot; style=&quot;-qt-table-type: root; margin-top:4px; margin-bottom:4px; margin-left:4px; margin-right:4px;&quot;&gt;
+&lt;tr&gt;
+&lt;td style=&quot;border: none;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;img src=&quot;:/maurina/Resources/about.jpg&quot; /&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Ubuntu&apos;; font-size:10pt; font-weight:600;&quot;&gt;Maurina&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;a href=&quot;http://www.maurina.org&quot;&gt;&lt;span style=&quot; font-family:&apos;Ubuntu&apos;; font-size:10pt; text-decoration: underline; color:#0000ff;&quot;&gt;http://www.maurina.org&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;v1.2&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;Código - Álvaro Calleja (&lt;/span&gt;&lt;a href=&quot;mailto:alvaro.calleja@gmail.com&quot;&gt;&lt;span style=&quot; font-family:&apos;Ubuntu&apos;; font-size:10pt; text-decoration: underline; color:#0000ff;&quot;&gt;alvaro.calleja@gmail.com&lt;/span&gt;&lt;/a&gt;&lt;span style=&quot; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;)&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;En recuerdo a la memoria de mi madre Maurina, a quien echo de menos a diario.&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;Este programa es libre y gratuito&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;Si encuentras útil este programa, por favor considera &lt;/span&gt;&lt;a href=&quot;http://www.maurina.org/donate&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; text-decoration: underline; color:#0000ff;&quot;&gt;hacer una donación&lt;/span&gt;&lt;/a&gt;&lt;span style=&quot; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;.&lt;/span&gt;&lt;/p&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
@@ -204,7 +222,7 @@ p, li { white-space: pre-wrap; }
 &lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;This software is free as in free beer and free speech.&lt;/span&gt;&lt;/p&gt;
 &lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
 &lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;If you find this program useful please consider &lt;/span&gt;&lt;a href=&quot;http://www.maurina.org/donate&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;making a donation&lt;/span&gt;&lt;/a&gt;&lt;span style=&quot; font-family:&apos;Ubuntu&apos;; font-size:10pt;&quot;&gt;.&lt;/span&gt;&lt;/p&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="vanished">&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
@@ -235,72 +253,72 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="../configWindow.ui" line="14"/>
         <source>Preferences</source>
-        <translation type="unfinished"></translation>
+        <translation>Preferencias</translation>
     </message>
     <message>
         <location filename="../configWindow.ui" line="24"/>
         <source>Server configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>Configuración del servidor</translation>
     </message>
     <message>
         <location filename="../configWindow.ui" line="32"/>
         <source>Address</source>
-        <translation type="unfinished"></translation>
+        <translation>Dirección IP</translation>
     </message>
     <message>
         <location filename="../configWindow.ui" line="49"/>
         <source>Port</source>
-        <translation type="unfinished">Puerto</translation>
+        <translation>Puerto</translation>
     </message>
     <message>
         <location filename="../configWindow.ui" line="87"/>
         <source>Messages styles</source>
-        <translation type="unfinished"></translation>
+        <translation>Estilo de mensajes</translation>
     </message>
     <message>
         <location filename="../configWindow.ui" line="96"/>
         <source>time</source>
-        <translation type="unfinished"></translation>
+        <translation>time</translation>
     </message>
     <message>
         <location filename="../configWindow.ui" line="106"/>
         <source>h1</source>
-        <translation type="unfinished"></translation>
+        <translation>h1</translation>
     </message>
     <message>
         <location filename="../configWindow.ui" line="113"/>
         <source>h2</source>
-        <translation type="unfinished"></translation>
+        <translation>h2</translation>
     </message>
     <message>
         <location filename="../configWindow.ui" line="126"/>
         <source>h3</source>
-        <translation type="unfinished"></translation>
+        <translation>h3</translation>
     </message>
     <message>
         <location filename="../configWindow.ui" line="136"/>
         <source>h4</source>
-        <translation type="unfinished"></translation>
+        <translation>h4</translation>
     </message>
     <message>
         <location filename="../configWindow.ui" line="146"/>
         <source>pre</source>
-        <translation type="unfinished"></translation>
+        <translation>pre</translation>
     </message>
     <message>
         <location filename="../configWindow.ui" line="156"/>
         <source>var</source>
-        <translation type="unfinished"></translation>
+        <translation>var</translation>
     </message>
     <message>
         <location filename="../configWindow.ui" line="166"/>
         <source>h5</source>
-        <translation type="unfinished"></translation>
+        <translation>h5</translation>
     </message>
     <message>
         <location filename="../configWindow.ui" line="176"/>
         <source>h6</source>
-        <translation type="unfinished"></translation>
+        <translation>h6</translation>
     </message>
 </context>
 </TS>
